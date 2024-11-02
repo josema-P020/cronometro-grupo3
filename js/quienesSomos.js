f6da4b0a92e57996ed86f9c4df3d3957bad8f419
jose maria perez <josema.perez.020@gmail.com>
2024-11-01 22:53:50 -0300

    agregar class de los integrantes

---
+++ js/quienesSomos.js
@@ -0,0 +1,9 @@
+class integrantes {
+    constructor (foto, nombre, edad, descripcion, id) {
+        this.foto = foto;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.descripcion = descripcion;
+        this.id= id;
+    }
+}
